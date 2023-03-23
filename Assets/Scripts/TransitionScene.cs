190c648725ff23f957ab16c84154c93ee7af1d34
TrevorMcK <73631034+TrevorMcK@users.noreply.github.com>
2023-03-23 11:48:07 -0500

    UI and Button Functionality for Menu Scenes
    - Added UI:
       - Selection Scene
       - Settings Scene
    - Fixed Transition Script to work with just indexes.
    - Added Button Functionality:
       - Exit
       - Start
       - Return
       - Options

--- Assets/Scripts/TransitionScene.cs
+++ Assets/Scripts/TransitionScene.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Transition Scene Function.
+/// </summary>
+[System.Serializable]
 public class TransitionScene : MonoBehaviour
 {
-    public List<Scene> scene = new List<Scene>();
-
     /// <summary>
-    /// Changes between base on Scene index from scene list
+    /// Changes between base on Scene index from scene list.
     /// </summary>
-    /// <param name="index">Index of the Scene</param>
+    /// <param name="index">Index of the next Scene</param>
     public void ChangeScene(int index)
     {
-        SceneManager.LoadScene(scene[index].name);
+        SceneManager.LoadScene(index);
     }
 }
