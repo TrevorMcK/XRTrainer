eb21e088af929f0217065588c0124f822d371812
TrevorMcK <73631034+TrevorMcK@users.noreply.github.com>
2023-04-17 19:14:27 -0500

    Menu added for task

---
+++ Assets/Main/Scripts/TaskController.cs
@@ -0,0 +1,69 @@
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.XR.CoreUtils;
+using UnityEngine.UI;
+using System.IO;
+using UnityEditor;
+
+public class TaskController : MonoBehaviour
+{
+    [SerializeField] private TextAsset taskJson;
+    [SerializeField] private TMP_Text steps;
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private Button next;
+    [SerializeField] private Button back;
+
+    Dictionary<Task, GameObject> tasks = new Dictionary<Task, GameObject>();
+    private Tasklist myTasks = new Tasklist();
+    private int indexTasklist = 0;
+    //public static TaskController Singleton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTasks = JsonConvert.DeserializeObject<Tasklist>(taskJson.ToString());
+        setupTasks();
+        //Singleton = this;
+
+    }
+
+    private void setupTasks()
+    {
+        title.text = myTasks.task[indexTasklist].name;
+        steps.text = myTasks.task[indexTasklist].name;
+        greyOutButton(next, back);
+    }
+    public void gothroughTasks(bool nextBack = false)
+    {
+        indexTasklist = indexTasklist + (nextBack ? +1 : -1);
+        title.text = myTasks.task[indexTasklist].name;
+        steps.text = myTasks.task[indexTasklist].name;
+
+        greyOutButton(next, back);
+    }
+    
+    private void greyOutButton(Button btnNext, Button btnBack)
+    {
+        if(indexTasklist  <= 0)
+        {
+            btnBack.enabled = false;
+        }
+        else
+        {
+            btnBack.enabled = true;
+        }
+
+        if (indexTasklist >= myTasks.task.Length - 1)
+        {
+            btnNext.enabled = false;
+        }
+        else 
+        {
+            btnNext.enabled = true;
+        }
+
+    }
+}
--- Assets/Main/Scripts/TraineeController.cs
+++ Assets/Main/Scripts/TraineeController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class TraineeController : MonoBehaviour
 {
+    [SerializeField] private TextAsset[] taskConfig;
+    private Tasklist tasklist = new Tasklist();
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
