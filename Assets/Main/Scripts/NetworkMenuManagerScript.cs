862afdb7c2e6bb6093e11f6b146f0535c890f77f
HunterSpoon <hotrodspoon@gmail.com>
2023-04-20 19:20:13 -0500

    Network Implementation Complete
    Network currently runs on local host, going forward, we just need to add Network Object Components to anything we want to be transmitted between players.

--- Assets/Main/Scripts/NetworkMenuManagerScript.cs
+++ Assets/Main/Scripts/NetworkMenuManagerScript.cs
@@ -2,16 +2,25 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkMenuManagerScript: MonoBehaviour
-{ 
+{
+    public GameObject MainCamera; //the MainCamera
+
+    private void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera");//set the reference to the main camera
+    }
 
     public void OnClickHost()
     {
-        if (NetworkManager.Singleton == null)
+        if (NetworkManager.Singleton == null) //if a network manager does not exist
         {
             Debug.Log("No Network manager present, cannot start as host");
             return;
         }
-        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.StartHost(); //start the host
+        GameObject.Find("Canvas").GetComponent<CanvasGroup>().alpha = 0; //hide the canvas with the menu on it
+        MainCamera.SetActive(false); //hide the main camera, switching us to the player prefab camera
+        
     }
 
     public void OnClickJoin()
@@ -22,6 +31,8 @@ public class NetworkMenuManagerScript: MonoBehaviour
             return;
         }
         NetworkManager.Singleton.StartClient();
+        GameObject.Find("Canvas").GetComponent<CanvasGroup>().alpha = 0; //hide the canvas with the menu on it
+        MainCamera.SetActive(false); //hide the main camera, switching us to the player prefab camera
 
     }
 }
--- Assets/Main/Scripts/PlayerMovement.cs
+++ Assets/Main/Scripts/PlayerMovement.cs
@@ -4,23 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+        if (Input.GetKeyDown("space")){
+            rb.velocity = new Vector3(0, 5, 0);
         }
 
-        if (Input.GetKey("up"))
+        if (Input.GetKey("up")){
+            rb.velocity = new Vector3(0, 0, 5);
+        }
+
+        if (Input.GetKey("down"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+            rb.velocity = new Vector3(0, 0, -5);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            rb.velocity = new Vector3(-5, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            rb.velocity = new Vector3(5, 0, 0);
         }
     }
 }
