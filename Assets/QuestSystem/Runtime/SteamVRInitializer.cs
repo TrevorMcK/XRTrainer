9fc4593922f0f0105dbf0073ae544c2b9cd8af41
Trevor McKeown <trevormckeown99@gmail.com>
2023-11-16 20:12:06 -0600

    The init of xr is causing a crash

---
+++ Assets/QuestSystem/Runtime/SteamVRInitializer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Management;
+using Valve.VR;
+
+public class SteamVRInitializer : MonoBehaviour
+{
+    public UnityEvent OnSteamVRInitialized;
+
+    private void OnEnable()
+    {
+        StartCoroutine(InitRoutine());
+        Application.quitting += () =>
+        {
+            XRGeneralSettings.Instance.Manager.StopSubsystems();
+            XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        };
+    }
+
+    private IEnumerator InitRoutine()
+    {
+        XRGeneralSettings.Instance.Manager.InitializeLoader();
+        
+
+        yield return new WaitForSeconds(1);
+        Debug.Log("TEST: "+XRGeneralSettings.Instance.Manager.isInitializationComplete);
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("XR initialization failed!");
+        }
+        else
+        {
+            Debug.Log("Starting XR...");
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+        }
+
+        yield return null;
+        yield return null;
+
+        SteamVR.Initialize();
+
+
+        yield return new WaitWhile(() => SteamVR.initializedState == SteamVR.InitializedStates.Initializing);
+        yield return null;
+
+        OnSteamVRInitialized?.Invoke();
+    }
+}
